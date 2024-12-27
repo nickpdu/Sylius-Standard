7ba1984c28dc6bc50e2d2f14c4dd480b958952ee
nickpdu <pecknicholi@gmail.com>
2024-12-27 15:05:17 -0800

    remove js redundancy, add cart item validation

--- assets/shop/entrypoint.js
+++ assets/shop/entrypoint.js
@@ -1,2 +1,13 @@
 // In this file you can import assets like images or stylesheets
 console.log('Hello Webpack Encore! Edit me in assets/shop/entrypoint.js');
+
+var quantityInput = document.querySelector(
+  '.qnty-alert input, input[name="sylius_shop_add_to_cart[cartItem][quantity]"]'
+);
+
+quantityInput.addEventListener('change', function() {
+  var newValue = quantityInput.value;
+  if(newValue == 70){
+    alert('Great Choice!');
+  }
+});
